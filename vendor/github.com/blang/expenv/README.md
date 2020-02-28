dd7b594b9897f301085fff9f421300c7380343c5
yangjianghua <yangjianghua@didiglobal.com>
2020-02-28 11:44:30 +0800

    go expenv

---
+++ vendor/github.com/blang/expenv/README.md
@@ -0,0 +1,53 @@
+expenv - Expand environment variables [![Build Status](https://travis-ci.org/blang/expenv.svg?branch=master)](https://travis-ci.org/blang/expenv)
+======
+Expenv replaces ${var} or $var in the input to the values of the current environment variables. Supports stdin/stdout and files.
+
+There is also a newer version available, [expenv.sh](https://github.com/blang/expenv.sh), written as shell script.
+
+Usage
+-----
+```bash
+$ go get github.com/blang/expenv
+$ $GOPATH/bin/expenv -f inputfile
+```
+
+If you don't want to build it, you might want to download the [ELF x86-64 binary](https://github.com/blang/expenv/releases/latest) of the latest release (build by travis-ci).
+
+Examples
+-----
+
+```bash
+$ expenv -f inputfile > outputfile
+$ expenv < inputfile > outputfile
+$ expenv -i -f inputfile // Replace inplace
+```
+
+Example input:
+```
+My PWD is $PWD
+Whoami: ${USER}
+I'm using $TERM
+Expand $empty but don't expand $$empty
+Don't expand $(echo empty)
+```
+
+As mentioned in the above example, two special cases exist:
+
+* `$$` is converted to `$`, allowing you to pass in literal-dollar-signs
+* `$(` is not converted, allowing your input to contain subshell like behaviour
+
+Motivation
+-----
+
+I need to make config files more dynamic using environment variables. In a docker environment this is a big issue for me.
+
+Contribution
+-----
+
+Feel free to make a pull request. For bigger changes create a issue first to discuss about it.
+
+
+License
+-----
+
+See [LICENSE](LICENSE) file.
---
+++ vendor/github.com/blang/expenv/expenv.go
@@ -0,0 +1,144 @@
+package main
+
+import (
+	"bufio"
+	"flag"
+	"fmt"
+	"io"
+	"io/ioutil"
+	"os"
+	"strings"
+)
+
+func main() {
+	argFile := flag.String("f", "", "Input file, otherwise stdin")
+	argInPlace := flag.Bool("i", false, "Replace file inplace")
+	flag.Parse()
+
+	var input io.ReadCloser
+	var output io.WriteCloser
+	var err error
+
+	defer func() {
+		if output != nil {
+			if err := output.Close(); err != nil {
+				fmt.Fprintf(os.Stderr, "Error: %s\n", err)
+			}
+		}
+	}()
+
+	defer func() {
+		if input != nil {
+			if err := input.Close(); err != nil {
+				fmt.Fprintf(os.Stderr, "Error: %s\n", err)
+			}
+		}
+	}()
+
+	if *argFile != "" {
+		input, err = os.Open(*argFile)
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "Error input file: %s\n", err)
+			os.Exit(1)
+		}
+	} else {
+		input = ReadNopCloser(os.Stdin)
+	}
+
+	if *argInPlace && *argFile != "" {
+		file, err := ioutil.TempFile("", "expenv")
+		output = FileReplaceWriteCloser(file, *argFile)
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "Error output file: %s\n", err)
+			os.Exit(1)
+		}
+	} else {
+		output = WriteNopCloser(os.Stdout)
+	}
+
+	err = process(input, output)
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "Error: %s\n", err)
+		os.Exit(1)
+	}
+
+}
+
+// process reads line by line from reader, expands environment variables and writes lines to writer
+func process(r io.Reader, w io.Writer) error {
+	bufr := bufio.NewReader(r)
+	for {
+		line, err := bufr.ReadString('\n')
+		line = expandEnv(line)
+		_, writererr := io.WriteString(w, line)
+		if writererr != nil {
+			return err
+		}
+		if err != nil {
+			break
+		}
+	}
+	return nil
+}
+
+// Nop Closer for io.Reader
+type readNopCloser struct {
+	io.Reader
+}
+
+func (r readNopCloser) Close() error {
+	return nil
+}
+
+func ReadNopCloser(r io.Reader) io.ReadCloser {
+	return readNopCloser{r}
+}
+
+// Nop Closer for io.Writer
+type writeNopCloser struct {
+	io.Writer
+}
+
+func (r writeNopCloser) Close() error {
+	return nil
+}
+
+func WriteNopCloser(w io.Writer) io.WriteCloser {
+	return writeNopCloser{w}
+}
+
+// fileReplaceWriteCloser replaces the replacePath with given file on Close()
+type fileReplaceWriteCloser struct {
+	*os.File
+	replacePath string
+}
+
+// Close closes the file and renames it to replace path. If an error occurs the file is removed.
+func (f fileReplaceWriteCloser) Close() error {
+	err := f.File.Close()
+	if err != nil {
+		os.Remove(f.File.Name())
+		return err
+	}
+	err = os.Rename(f.File.Name(), f.replacePath)
+	if err != nil {
+		os.Remove(f.File.Name())
+		return err
+	}
+	return nil
+}
+
+func FileReplaceWriteCloser(f *os.File, replacePath string) io.WriteCloser {
+	return &fileReplaceWriteCloser{f, replacePath}
+}
+
+// Follow os.ExpandEnv's contract except for `$$` which is transformed to `$`
+func expandEnv(s string) string {
+	os.Setenv("EXPENV_DOLLAR", "$")
+	os.Setenv("EXPENV_PARENTHESIS", "$(")
+
+	s = strings.Replace(s, "$$", "${EXPENV_DOLLAR}", -1)
+	s = strings.Replace(s, "$(", "${EXPENV_PARENTHESIS}", -1)
+
+	return os.ExpandEnv(s)
+}
